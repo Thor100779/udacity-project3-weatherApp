d40ada2b86fc1f3f56286b568c657cc42a632ca2
Thor100779 <grot0130@umn.edu>
2021-10-06 08:05:36 -0500

    Make client receive data from server.

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// Setup empty JS ARRAY to act as endpoint for all routes
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 8000;
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
+const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of looking for a country code too
+const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +19,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+app.listen(port, () => {
+  console.log(`listening at localhost:${port}`);
+});
+
+app.get('/all', (req, res) => {
+  console.log(`ALL route invoked, sending projectData: ${projectData}`);
+  res.send(projectData);
+});
+
+app.get('/last', (req, res) => {
+  console.log(`LAST route invoked, sending projectData: ${projectData}`);
+
+  if (projectData.length >= 1) {
+    console.log('In /last route and projectData is NOT empty');
+    res.send(projectData[projectData.length - 1]);
+  } else {
+    console.log('In /last route and projectData IS empty');
+    res.send('No data to send');
+  }
+});
+
+// Expecting body to contain the following fields: zipCode, feelings
+app.post('/add', (req, res) => {
+  console.log(`server.js /add route, req.body: zip = ${req.body.zipCode}, feelings = ${req.body.feelings}`);
+  //res.send('Hello from the /add route!');
+  projectData.push({ "zipCode": `${req.body.zipCode}`, "feelings": `${req.body.feelings}`});
+  console.log(`projectData is: ${JSON.stringify(projectData)}`);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,95 @@
 /* Global Variables */
+const zipCodeField = document.getElementById('zip');
+const feelingsField = document.getElementById('feelings');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate2 = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`;
+
+document.getElementById('generate').addEventListener('click', generateButtonClickedHandler);
+
+function generateButtonClickedHandler() {
+  if (isEmpty(zipCodeField.value))
+    alert('You need to enter a zip code');
+  else if (isEmpty(feelingsField.value))
+    alert('You need to enter your feelings');
+  else {
+    console.log(`In button click handler and zip = ${zipCodeField.value}, feelings = ${feelingsField.value}`);
+    postData('/add', { "zipCode": `${zipCodeField.value}`, "feelings": `${feelingsField.value}` });
+    logFetch('/last').then(result => {
+      if (result === null)
+        console.log('result is NULL in button click handler');
+      else
+        document.getElementById('content').innerText = `The most recent zip is: ${result.zipCode},
+          the most recent feelings are: ${result.feelings}`;
+    });
+  }
+}
+
+function logFetch(url) {
+  return fetch(url)
+    //.then(response => response.text())
+    .then(response => response.json())
+    .then(json => {
+      console.log(`json inside logFetch is: ${JSON.stringify(json)}`);
+      return json;
+    }).catch(err => {
+      console.error('fetch failed', err);
+    });
+}
+
+const postData = async (url = '', data = {}) => {
+    console.log(`Inside postData, url = ${url}, data = ${JSON.stringify(data)}`);
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    console.log('inside postData still, but before try block');
+    try {
+      console.log(`In postData try block, data posted was: ${JSON.stringify(data)}`);
+      const rcvData = await response.json();
+      console.log(`In postData try block, data received was: ${JSON.stringify(rcvData)}`);
+      return JSON.stringify(rcvData);
+    } catch(error) {
+      if (error === null)
+        console.log("There was an error");
+      else
+        console.log("error was NOT null, but I'm having trouble logging the error");
+    }
+}
+
+const retrieveData = async (url = '') => {
+  /*fetch(url).then(response => response.json()).then(data => {
+    console.log(`retrieved zip = ${data.zip}, feelings = ${data.feelings}`);
+  });*/
+  console.log(`In retrieveData, about to call fetch(${url})`);
+  const request = await fetch(url);
+
+  try {
+    console.log('retrieveData, before await request');
+    const data = await request.json();
+    console.log(`retrieveData, data is: ${data}`);
+    //return data;
+  } catch(error) {
+    console.log(`${error.message}`);
+  }
+}
+
+function isEmpty(str) {
+  return !str.trim().length;
+}
+
+function postGet(data = {}) {
+  console.log('in postGet...');
+  postData('/add', data).then(function(result) {
+    console.log('about to call retrieveData');
+    retrieveData('/last');
+    console.log('after retrieveData was called');
+  });
+  console.log('...done in postGet');
+}
