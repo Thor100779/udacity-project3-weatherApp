891b24e4909702b5d6f98ec4d2885e1339aa35eb
Thor100779 <grot0130@umn.edu>
2021-10-07 16:25:01 -0500

    Add new styling.

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 8000;
-const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
+/*const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
 const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of looking for a country code too
-const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';
+const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';*/
 
 // Start up an instance of app
 const app = express();
@@ -29,10 +29,37 @@ app.listen(port, () => {
   console.log(`listening at localhost:${port}`);
 });
 
-app.get('/all', (req, res) => {
+/*app.get('/all', (req, res) => {
   console.log(`ALL route invoked, sending projectData: ${projectData}`);
   res.send(projectData);
-});
+});*/
+
+/*function logFetch(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      console.log(`json inside logFetch is: ${JSON.stringify(json)}`);
+      return json;
+    }).catch(err => {
+      console.error('fetch failed', err);
+    });
+}*/
+
+/*function getTempForZipCode(zipCode) {
+  logFetch(baseUrl + zipCode + midUrl + apiKey)
+    .then(result => {
+      let tempKelvin = result.main.temp;
+      console.log(`tempKelvin is: ${tempKelvin}`);
+      return (tempKelvin - 273.15) * (9 / 5) + 32;
+    });
+}*/
+
+/*app.get('/temp', (req, res) => {
+  //let zip = req.body.zipCode;
+  let temp = getTempForZipCode(90210);
+  console.log(`in app.get('/temp') and temp found to be: ${temp}`);
+  res.send({"temp": temp});
+});*/
 
 app.get('/last', (req, res) => {
   console.log(`LAST route invoked, sending projectData: ${projectData}`);
@@ -51,5 +78,5 @@ app.post('/add', (req, res) => {
   console.log(`server.js /add route, req.body: zip = ${req.body.zipCode}, feelings = ${req.body.feelings}`);
   //res.send('Hello from the /add route!');
   projectData.push({ "zipCode": `${req.body.zipCode}`, "feelings": `${req.body.feelings}`});
-  console.log(`projectData is: ${JSON.stringify(projectData)}`);
+  //console.log(`projectData is: ${JSON.stringify(projectData)}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,11 +1,10 @@
 /* Global Variables */
-const zipCodeField = document.getElementById('zip');
-const feelingsField = document.getElementById('feelings');
+const zipCodeField = document.getElementById('zip_code_field');
+const feelingsField = document.getElementById('feelings_field');
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-let newDate2 = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`;
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of looking for a country code too
+const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';
 
 document.getElementById('generate').addEventListener('click', generateButtonClickedHandler);
 
@@ -20,11 +19,58 @@ function generateButtonClickedHandler() {
     logFetch('/last').then(result => {
       if (result === null)
         console.log('result is NULL in button click handler');
-      else
-        document.getElementById('content').innerText = `The most recent zip is: ${result.zipCode},
-          the most recent feelings are: ${result.feelings}`;
+      else {
+        document.getElementById('content').innerText = `The most recent zip is: ${result.zipCode}\n
+          The most recent feelings are: ${result.feelings}`;
+        document.getElementById('date').innerText = `Date: ${getDate()}`;
+      }
     });
+
+    logFetch(baseUrl + zipCodeField.value + midUrl + apiKey)
+    .then(temp => {
+      if (temp === undefined)
+        console.log('final temp is UNDEFINED');
+      else {
+        console.log(`final temp is: ${temp.main.temp}`);
+        let tempF = kelvinToFahrenheit(temp.main.temp);
+        document.getElementById('temp').innerText = `Temp: ${tempF}` + '\xB0' + 'F';
+      }
+    })
+    .catch(err => {
+      console.error('fetch failed', err);
+    });
+
+    /*getTempForZipCode(zipCodeField.value).then(temp => {
+      if (temp === null)
+        console.log("app.js failed to get temp");
+      else {
+        console.log(`Temp is not null and equals: ${temp}`);
+        document.getElementById('temp').innerText = `Temp: ${temp}`;
+      }
+    });*/
   }
+}
+
+function kelvinToFahrenheit(tempInKelvin) {
+  return Math.trunc((tempInKelvin - 273.15) * (9 / 5) + 32);
+}
+
+function getTempForZipCode(zipCode) {
+  console.log(`fetching temp for zip: ${zipCode}`);
+  logFetch(baseUrl + zipCode + midUrl + apiKey)
+    .then(result => {
+      console.log(`returned result for url: ${baseUrl + zipCode + midUrl + apiKey} is: ${result}`);
+      let tempKelvin = result.main.temp;
+      console.log(`tempKelvin is: ${tempKelvin}`);
+      return (tempKelvin - 273.15) * (9 / 5) + 32;
+    });
+}
+
+function getDate() {
+  // Create a new date instance dynamically with JS
+  let d = new Date();
+  //let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+  return `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`;
 }
 
 function logFetch(url) {
