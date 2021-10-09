f37f651ea2510ff5de4df217f402819ffe5f4650
Thor100779 <grot0130@umn.edu>
2021-10-09 13:27:23 -0500

    Improve styling. Remove debugging statements.

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 8000;
-/*const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
-const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of looking for a country code too
-const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';*/
 
 // Start up an instance of app
 const app = express();
@@ -29,54 +26,14 @@ app.listen(port, () => {
   console.log(`listening at localhost:${port}`);
 });
 
-/*app.get('/all', (req, res) => {
-  console.log(`ALL route invoked, sending projectData: ${projectData}`);
-  res.send(projectData);
-});*/
-
-/*function logFetch(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .then(json => {
-      console.log(`json inside logFetch is: ${JSON.stringify(json)}`);
-      return json;
-    }).catch(err => {
-      console.error('fetch failed', err);
-    });
-}*/
-
-/*function getTempForZipCode(zipCode) {
-  logFetch(baseUrl + zipCode + midUrl + apiKey)
-    .then(result => {
-      let tempKelvin = result.main.temp;
-      console.log(`tempKelvin is: ${tempKelvin}`);
-      return (tempKelvin - 273.15) * (9 / 5) + 32;
-    });
-}*/
-
-/*app.get('/temp', (req, res) => {
-  //let zip = req.body.zipCode;
-  let temp = getTempForZipCode(90210);
-  console.log(`in app.get('/temp') and temp found to be: ${temp}`);
-  res.send({"temp": temp});
-});*/
-
 app.get('/last', (req, res) => {
-  console.log(`LAST route invoked, sending projectData: ${projectData}`);
-
-  if (projectData.length >= 1) {
-    console.log('In /last route and projectData is NOT empty');
+  if (projectData.length >= 1)
     res.send(projectData[projectData.length - 1]);
-  } else {
-    console.log('In /last route and projectData IS empty');
+  else
     res.send('No data to send');
-  }
 });
 
 // Expecting body to contain the following fields: zipCode, feelings
 app.post('/add', (req, res) => {
-  console.log(`server.js /add route, req.body: zip = ${req.body.zipCode}, feelings = ${req.body.feelings}`);
-  //res.send('Hello from the /add route!');
   projectData.push({ "zipCode": `${req.body.zipCode}`, "feelings": `${req.body.feelings}`});
-  //console.log(`projectData is: ${JSON.stringify(projectData)}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,53 +1,34 @@
-/* Global Variables */
-const zipCodeField = document.getElementById('zip_code_field');
-const feelingsField = document.getElementById('feelings_field');
+// Global Variables
+const zipCodeField = document.getElementById('zip');
+const feelingsField = document.getElementById('feelings');
 
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of looking for a country code too
+const midUrl = ',us&appid=';  // *Assuming we only need to cover the United States, I added the 'us' instead of asking for a country code too
 const apiKey = 'a53b2d8dc37f7179eb7025f975cc83b2';
 
 document.getElementById('generate').addEventListener('click', generateButtonClickedHandler);
 
 function generateButtonClickedHandler() {
-  if (isEmpty(zipCodeField.value))
-    alert('You need to enter a zip code');
+  if (isEmpty(zipCodeField.value) || zipCodeField.value.length != 5)
+    alert('You need to enter a valid 5 digit zip code');
   else if (isEmpty(feelingsField.value))
     alert('You need to enter your feelings');
   else {
-    console.log(`In button click handler and zip = ${zipCodeField.value}, feelings = ${feelingsField.value}`);
     postData('/add', { "zipCode": `${zipCodeField.value}`, "feelings": `${feelingsField.value}` });
-    logFetch('/last').then(result => {
-      if (result === null)
-        console.log('result is NULL in button click handler');
-      else {
-        document.getElementById('content').innerText = `The most recent zip is: ${result.zipCode}\n
-          The most recent feelings are: ${result.feelings}`;
+    fetchJson('/last').then(result => {
+        document.getElementById('content').innerText = `Zip code: ${result.zipCode}\n
+          Feelings: ${result.feelings}`;
         document.getElementById('date').innerText = `Date: ${getDate()}`;
-      }
     });
 
-    logFetch(baseUrl + zipCodeField.value + midUrl + apiKey)
+    fetchJson(baseUrl + zipCodeField.value + midUrl + apiKey)
     .then(temp => {
-      if (temp === undefined)
-        console.log('final temp is UNDEFINED');
-      else {
-        console.log(`final temp is: ${temp.main.temp}`);
         let tempF = kelvinToFahrenheit(temp.main.temp);
-        document.getElementById('temp').innerText = `Temp: ${tempF}` + '\xB0' + 'F';
-      }
+        document.getElementById('temp').innerText = `Temp: ${tempF}` + '\xB0' + 'F';  // Add indication
     })
     .catch(err => {
       console.error('fetch failed', err);
     });
-
-    /*getTempForZipCode(zipCodeField.value).then(temp => {
-      if (temp === null)
-        console.log("app.js failed to get temp");
-      else {
-        console.log(`Temp is not null and equals: ${temp}`);
-        document.getElementById('temp').innerText = `Temp: ${temp}`;
-      }
-    });*/
   }
 }
 
@@ -55,38 +36,20 @@ function kelvinToFahrenheit(tempInKelvin) {
   return Math.trunc((tempInKelvin - 273.15) * (9 / 5) + 32);
 }
 
-function getTempForZipCode(zipCode) {
-  console.log(`fetching temp for zip: ${zipCode}`);
-  logFetch(baseUrl + zipCode + midUrl + apiKey)
-    .then(result => {
-      console.log(`returned result for url: ${baseUrl + zipCode + midUrl + apiKey} is: ${result}`);
-      let tempKelvin = result.main.temp;
-      console.log(`tempKelvin is: ${tempKelvin}`);
-      return (tempKelvin - 273.15) * (9 / 5) + 32;
-    });
-}
-
 function getDate() {
-  // Create a new date instance dynamically with JS
-  let d = new Date();
-  //let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+  let d = new Date(); // Create a new date instance dynamically with JS
   return `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`;
 }
 
-function logFetch(url) {
+function fetchJson(url) {
   return fetch(url)
-    //.then(response => response.text())
     .then(response => response.json())
-    .then(json => {
-      console.log(`json inside logFetch is: ${JSON.stringify(json)}`);
-      return json;
-    }).catch(err => {
+    .catch(err => {
       console.error('fetch failed', err);
     });
 }
 
 const postData = async (url = '', data = {}) => {
-    console.log(`Inside postData, url = ${url}, data = ${JSON.stringify(data)}`);
     const response = await fetch(url, {
       method: 'POST',
       credentials: 'same-origin',
@@ -95,47 +58,15 @@ const postData = async (url = '', data = {}) => {
       },
       body: JSON.stringify(data),
     });
-    console.log('inside postData still, but before try block');
+
     try {
-      console.log(`In postData try block, data posted was: ${JSON.stringify(data)}`);
       const rcvData = await response.json();
-      console.log(`In postData try block, data received was: ${JSON.stringify(rcvData)}`);
       return JSON.stringify(rcvData);
     } catch(error) {
-      if (error === null)
-        console.log("There was an error");
-      else
-        console.log("error was NOT null, but I'm having trouble logging the error");
+        console.log(error);
     }
-}
-
-const retrieveData = async (url = '') => {
-  /*fetch(url).then(response => response.json()).then(data => {
-    console.log(`retrieved zip = ${data.zip}, feelings = ${data.feelings}`);
-  });*/
-  console.log(`In retrieveData, about to call fetch(${url})`);
-  const request = await fetch(url);
-
-  try {
-    console.log('retrieveData, before await request');
-    const data = await request.json();
-    console.log(`retrieveData, data is: ${data}`);
-    //return data;
-  } catch(error) {
-    console.log(`${error.message}`);
-  }
 }
 
 function isEmpty(str) {
   return !str.trim().length;
-}
-
-function postGet(data = {}) {
-  console.log('in postGet...');
-  postData('/add', data).then(function(result) {
-    console.log('about to call retrieveData');
-    retrieveData('/last');
-    console.log('after retrieveData was called');
-  });
-  console.log('...done in postGet');
 }
